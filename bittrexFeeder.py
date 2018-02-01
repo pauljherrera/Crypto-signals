5f23d072b0ab456c803f2044a34be8ec7a759484
luis <luisomar3@hotmail.com>
2018-02-01 17:42:48 -0400

    Added different version APIS, markets

--- bittrexFeeder.py
+++ bittrexFeeder.py
@@ -4,18 +4,21 @@ from datafeeder import DataFeeder
 from bittrex import Bittrex, API_V2_0, TICKINTERVAL_FIVEMIN
 # In order to get the ticks, we have to use V2 API
 
-my_Bittrex = Bittrex(None, None, api_version=API_V2_0)
+my_Bittrex_v2 = Bittrex(None, None, api_version=API_V2_0)
+my_Bittrex_v1 = Bittrex(None,None)
 #Initialize bittrex object.
 
 class BittrexFeeder(DataFeeder):
     
-    def __init__(self,coins):
+    def __init__(self):
         
 
         super().__init__()
-
-        data = coins
-        setattr(self,"Coins",data)
+        dict_markets= my_Bittrex_v1.get_markets()
+        markets = dict_markets['result']
+        df_markets = pd.DataFrame(markets)
+        market_names = df_markets['MarketName']
+        setattr(self,"Markets",market_names)
 
     def get_ticks(self,market,interval):
 
@@ -29,4 +32,6 @@ class BittrexFeeder(DataFeeder):
         df_DTOHLCV.index = pd.to_datetime(df_DTOHLCV.index)
 
         return df_DTOHLCV
+
+
 
