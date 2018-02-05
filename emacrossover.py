3e572aa85f92f7405d19eb3d69468ec3397a820d
luis <luisomar3@hotmail.com>
2018-02-05 12:58:57 -0400

    first version

--- emacrossover.py
+++ emacrossover.py
@@ -1,25 +1,40 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
 
 class EMACrossoverBullish(object):
+    """Class for EMA crossover calculation
+    """
+
     def __init__(self, data, slowEMA, fastEMA):
+        """Initialize parameters
+        :param data: DataFrame with DTOHLCV
+        :type data: pd.Dataframe
+
+        :param slowEMA: Slow window for EMA calculation
+        :param slowEMA: int.
+
+        :param fastEMA: Fast window for EMA calculation
+        :type fastEMA: int.
+        """
         self.slowEMA = slowEMA
-        self.fastEMA = fastEMA 
+        self.fastEMA = fastEMA
         self.data = data
 
-
-    def _ExpMovingAverage(self,values, window):
+    def _ExpMovingAverage(self, values, window):
         weights = np.exp(np.linspace(-1., 0., window))
         weights /= weights.sum()
-        a =  np.convolve(values, weights, mode='full')[:len(values)]
+        a = np.convolve(values, weights, mode='full')[:len(values)]
         a[:window] = a[window]
-        
+
         return a
 
-    def Crossover (self):
+    def Crossover(self):
         data = self.data
-        data['slowEma'] = self._ExpMovingAverage(data['C'],self.slowEMA)
-        data['fastEma'] = self._ExpMovingAverage(data['C'],self.fastEMA)
-        data['Crossover'] = np.where(data['slowEma']>data['fastEma'],1,0)
-        return data
+        data['slowEma'] = self._ExpMovingAverage(data['C'], self.slowEMA)
+        data['fastEma'] = self._ExpMovingAverage(data['C'], self.fastEMA)
+
+        data['crossover'] = np.where((data['slowEma'] > data['fastEma']) & (data['slowEma'].shift(
+            1) <= data['fastEma'].shift(1)),1,0)
+
+        return data[]
