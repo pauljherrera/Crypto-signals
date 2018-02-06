ffca531e67956da3a3f19f3a5d5daafda67aa32f
luis <luisomar3@hotmail.com>
2018-02-06 08:29:47 -0400

    fix scope

--- quicktest.py
+++ quicktest.py
@@ -10,10 +10,10 @@ start_time = time.time()
 def main():
     while True:
         
-        feeder = my_bittrex.Name
-
-        for_plot = my_strategy._crossover()
-        for_plot.plot(x=for_plot.index, y=['C', 'slowEma', 'fastEma'])
+        dtohlcv = my_bittrex.get_ticks()
+        #Get data ticks from bittrex
+        my_strategy = Strategy(feeder, data=dtohlcv, slowEMA=5,
+                           fastEMA=20, window=30, threshold=10)
         print(my_strategy.Opportunity())
         #plt.show()
         time.sleep(315)
@@ -25,10 +25,7 @@ if __name__ == '__main__':
     #Initialize BittrexFeeder
     feeder = my_bittrex.Name
     #Set Feeder name
-    dtohlcv = my_bittrex.get_ticks()
-    #Get data ticks from bittrex
-    my_strategy = Strategy(feeder, data=dtohlcv, slowEMA=5,
-                           fastEMA=20, window=30, threshold=10)
+   
     #Create the strategy with options from the user.
 
     main()
