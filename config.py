daccc58128e4c825d8bfb49ade376622eec6001f
luis <luisomar3@hotmail.com>
2018-02-09 17:27:36 -0400

    Config file

---
+++ config.py
@@ -0,0 +1,25 @@
+strategy_config = {
+    "destination_email" : ["luisomar242@gmail.com","luisomar242@gmail.com"]
+}
+
+
+bittrex_config = {
+
+    "interval" : 'fiveMin',       #intervals available on bittrex : oneMin, fiveMin ,thirtyMin, hour , Day
+    "markets" : None                             
+}
+
+
+
+volume_power_config = {
+
+    "window" : 20,
+    "threshold" : 10
+}
+
+Ema_crossover_config = {
+
+    "slowEma" : 5,
+    "fastEma" : 20
+
+}
